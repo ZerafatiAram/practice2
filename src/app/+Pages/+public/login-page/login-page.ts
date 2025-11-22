50cbc98c2ddd99966abfe6de02f815ab1c52e4e5
ZerafatiAram <kosarzaram@gmail.com>
2025-11-22 13:47:46 +0330

    update project

--- src/app/+Pages/+public/login-page/login-page.ts
+++ src/app/+Pages/+public/login-page/login-page.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
 export class LoginPage {
-
+  username:string='';
+  password:string='';
+  remember:boolean=false;
+  router=inject(Router)
+  massege:string='';
+  login(){
+    if (this.username=='kosar'&& this.password=='2390') {
+       this.router.navigateByUrl('/private');
+    } else {
+      this.massege=' نام کاربری یا پسورد غلط است'
+    }
+  }
 }
